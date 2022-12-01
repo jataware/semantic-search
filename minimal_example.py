e72f9853bdeb9df1e82ad33b8c4ccdc21a74eae2
David Samson <david.andrew.engineer@gmail.com>
2022-12-01 14:54:52 -0500

    created minimal example usage of search

---
+++ minimal_example.py
@@ -0,0 +1,18 @@
+from search.corpora import Indicators
+from search.bert_search import BertSearch
+
+
+# load a corpus of documents, and set up a search engine
+corpus = Indicators.get_corpus()
+engine = BertSearch(corpus)
+
+# perform the search
+query = 'number of people who have been vaccinated'
+matches = engine.search(query, n=10)
+
+# print the results
+print('Query:', query)
+print('Top 10 matches:')
+for match_id, score in matches:
+    raw_text = corpus[match_id]   #get the matching text for the given id
+    print(raw_text, end='\n\n\n') #print the text
