15c40151b19284fa8c314915862b4e4e99fb3314
David Samson <david.andrew.engineer@gmail.com>
2022-12-05 10:14:41 -0500

    cleaned up some comments

--- minimal_example.py
+++ minimal_example.py
@@ -15,14 +15,14 @@ print('Query:', query)
 embedding = engine.embed_query(query)
 print('embedding:\n', embedding, '\n')
 
-# perform the search
+# perform the search, and collect the top 10 matches
 matches = engine.search(query, n=10)
 
 # print the results of the search
 print('Top 10 matches:')
 for match_id, score in matches:
-    raw_text = corpus[match_id]   #get the matching text for the given id
-    print(raw_text, end='\n\n\n') #print the text
+    raw_text = corpus[match_id]   # get the matching text for the given id
+    print(raw_text, end='\n\n\n') # print the text
 
 
 
