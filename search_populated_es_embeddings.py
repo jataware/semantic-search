c27978dd3d2dc9f9a958b7aa871030cf25ac6ccd
Joel Quiles <joel@radcoding.com>
2022-12-13 11:00:39 -0500

    adding embedding search algo comparison

--- search_populated_es_embeddings.py
+++ search_populated_es_embeddings.py
@@ -18,7 +18,36 @@ print('Query:', query)
 # Embedding for input query
 embedding = engine.embed_query(query)
 
-es_query = {
+# If searchin datasets, with nested features within it (properties are really named datasets.outputs)
+datasets_query = {
+  "_source": [
+    "id",
+    "name"
+  ],
+  "size": 10,
+  "query": {
+    "nested": {
+      "inner_hits": {},
+      "path": "outputs",
+      "query": {
+        "script_score": {
+          "query": {
+            "match_all": {}
+          },
+          "script": {
+            "source": "cosineSimilarity(params.query_vector, 'outputs.embeddings') + 1.0",
+            "params": {
+                "query_vector": embedding.tolist()
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# If searchin features as a top-level index
+features_query = {
     "query": {
         "script_score": {
             "query": {"match_all": {}},
@@ -41,6 +70,7 @@ es_query = {
     ]
 }
 
-results = json.dumps(es.search(index="features", body=es_query)["hits"]["hits"])
+results = json.dumps(es.search(index="features", body=features_query)["hits"]["hits"])
+# results = json.dumps(es.search(index="datasets", body=datasets_query)["hits"]["hits"])
 
 print(f"Results:\n{results}")
