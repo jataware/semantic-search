d3d62ec1e92584111bc5637f96bfb30eb4518f9b
David Samson <david.andrew.engineer@gmail.com>
2023-03-15 11:16:36 -0400

    added super simple repl for testing text summarization

---
+++ experiments/text_summarization.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import pipeline
+from easyrepl import REPL
+
+def main():
+    summarizer = pipeline(
+        "summarization",
+        "pszemraj/long-t5-tglobal-base-16384-book-summary",
+        device=0 if torch.cuda.is_available() else -1,
+    )
+    for line in REPL():
+        result = summarizer(line)[0]['summary_text']
+        print(f'||| {result}')
+
+if __name__ == "__main__":
+    main()
